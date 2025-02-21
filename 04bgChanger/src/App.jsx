ecb169d0088028b7aea3b660701a58b928557174
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-02-21 17:24:14 +0530

    bg changer Project

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,36 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState("Olive")
+  const [color, setColor] = useState("olive");
 
   return (
-    
-      <div class="w-full h-screen duration-200"
-      style={{backgroundColor : color}}
-      >
-        
-        <div class="flex flex-wrap  w-auto h-14 mt-40 bg-red-200 rounded-2xl px-2">
-            <button class="m-2 mx-3 border-black  bg-red-700 rounded-4xl" onClick={()=>setColor("Red")} >Red</button>
-            <button class="m-2 mx-3 border-black  bg-green-500 rounded-4xl " onClick={()=>setColor("Green")}>Green</button>
-            <button class="m-2 mx-3 border-black  bg-blue-500 rounded-4xl" onClick={()=>setColor("Blue")} > Blue</button>
-            <button class="m-2 mx-3 border-black  bg-green-800 rounded-4xl"onClick={()=>setColor("Olive")}>Olive</button>
-            <button class="m-2 mx-3 border-black  bg-gray-500 rounded-4xl"onClick={()=>setColor("Gray")}>Gray</button>
-            <button class="m-2 mx-3 border-black  bg-yellow-500 rounded-4xl"onClick={()=>setColor("Yellow")}>Yellow</button>
-            <button class="m-2 mx-3 border-black  bg-pink-500 rounded-4xl"onClick={()=>setColor("Pink")}>Pink</button>
-            <button class="m-2 mx-3 border-black  bg-purple-500 rounded-4xl"onClick={()=>setColor("Purple")}>Purple</button>
-            <button class="m-2 mx-3 border-black  bg-rose-400 rounded-4xl"onClick={()=>setColor("Lavender")}>Lavender</button>
-            <button class="m-2 mx-3 text-black border-black  bg-white rounded-4xl"onClick={()=>setColor("White")}>White</button>
-            <button class="m-2 mx-3 border-black  bg-black rounded-4xl"onClick={()=>setColor("Black")}>Black</button>
-
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
         </div>
-         
       </div>
-     
-    
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
